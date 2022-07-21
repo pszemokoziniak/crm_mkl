f21cd873f13218807b1e563cc786111dcdeaad4f
MarcinGladkowski <marcingladkowski@gmail.com>
2022-07-21 21:49:07 +0200

    documents: fix error displaying on file input

--- resources/js/Pages/CtnDocuments/Create.vue
+++ resources/js/Pages/CtnDocuments/Create.vue
@@ -9,7 +9,7 @@
       <form @submit.prevent="store">
         <div class="flex flex-wrap -mb-8 -mr-6 p-8">
           <text-input v-model="form.name" :error="form.errors.name" class="pb-8 pr-6 w-full lg:w-1/2" label="Nazwa" />
-          <file-input v-model="form.document" :errors="[form.errors.document]" class="pb-8 pr-6 w-full lg:w-1/2" type="file" accept="image/*" label="Dokument" />
+          <file-input v-model="form.document" :errors="form.errors.document" class="pb-8 pr-6 w-full lg:w-1/2" type="file" accept="image/*" label="Dokument" />
         </div>
         <div class="flex items-center justify-end px-8 py-4 bg-gray-50 border-t border-gray-100">
           <loading-button :loading="form.processing" class="btn-indigo" type="submit">Dodaj dokument</loading-button>
