b9006c774048b569e2d6743b37b0db3ac6787074
MarcinGladkowski <marcingladkowski@gmail.com>
2022-07-27 21:44:08 +0200

    time_sheets: add first migration

---
+++ database/migrations/2022_07_27_191006_building_work_time.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuildingWorkTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('building_work_time', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('organization_id')->index();
+            $table->unsignedInteger('contact_id');
+            $table->dateTimeTz('work_from');
+            $table->dateTimeTz('work_to');
+            $table->char('effective_work_time');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('building_work_time');
+    }
+}
