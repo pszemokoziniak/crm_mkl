7f788d5b9e40d3403b2535653eed42a5e6be572b
MarcinGladkowski <marcingladkowski@gmail.com>
2022-07-30 12:39:18 +0200

    time_sheets: cleanup html component

--- resources/js/Pages/Building/Index.vue
+++ resources/js/Pages/Building/Index.vue
@@ -1,107 +1,12 @@
 <template>
-
   <div class="bg-white rounded-lg shadow overflow-auto grid flex py-2 px-6">
     <div>
-      <span x-text="MONTH_NAMES[month]" class="text-lg font-bold text-gray-800">July</span>
-      <span x-text="year" class="ml-1 text-lg text-gray-600 font-normal">2022</span>
+      <span class="text-lg font-bold text-gray-800">July</span>
+      <span class="ml-1 text-lg text-gray-600 font-normal">2022</span>
     </div>
     <div class="flex border-t border-l">
-
       <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
-        <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
-      </div>
-      <div class="px-4 pt-2 border-r border-b relative" style="width: 222px; height: 120px;">
-        <div x-text="date" class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
+        <div class="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-700 hover:bg-blue-200 text-gray-700 hover:bg-blue-200">3</div>
         <div class="overflow-y-auto mt-1" style="height: 80px;"></div>
       </div>
     </div>
@@ -109,7 +14,6 @@
 </template>
 
 <script>
-
 import Layout from '@/Shared/Layout'
 
 export default {
@@ -121,134 +25,5 @@ export default {
   data() {
     return {}
   },
-  methods: {
-    isToday(date) {
-      const today = new Date();
-      const d = new Date(this.year, this.month, date)
-
-      return today.toDateString() === d.toDateString()
-    },
-  }
 }
-// const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-// const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//
-// function app() {
-//   return {
-//     month: '',
-//     year: '',
-//     no_of_days: [],
-//     blankdays: [],
-//     days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-//
-//     events: [
-//       {
-//         event_date: new Date(2020, 3, 1),
-//         event_title: "April Fool's Day",
-//         event_theme: 'blue'
-//       },
-//
-//       {
-//         event_date: new Date(2020, 3, 10),
-//         event_title: "Birthday",
-//         event_theme: 'red'
-//       },
-//
-//       {
-//         event_date: new Date(2020, 3, 16),
-//         event_title: "Upcoming Event",
-//         event_theme: 'green'
-//       }
-//     ],
-//     event_title: '',
-//     event_date: '',
-//     event_theme: 'blue',
-//
-//     themes: [
-//       {
-//         value: "blue",
-//         label: "Blue Theme"
-//       },
-//       {
-//         value: "red",
-//         label: "Red Theme"
-//       },
-//       {
-//         value: "yellow",
-//         label: "Yellow Theme"
-//       },
-//       {
-//         value: "green",
-//         label: "Green Theme"
-//       },
-//       {
-//         value: "purple",
-//         label: "Purple Theme"
-//       }
-//     ],
-//
-//     openEventModal: false,
-//
-//     initDate() {
-//       let today = new Date();
-//       this.month = today.getMonth();
-//       this.year = today.getFullYear();
-//       this.datepickerValue = new Date(this.year, this.month, today.getDate()).toDateString();
-//     },
-//
-//     isToday(date) {
-//       const today = new Date();
-//       const d = new Date(this.year, this.month, date);
-//
-//       return today.toDateString() === d.toDateString() ? true : false;
-//     },
-//
-//     showEventModal(date) {
-//       // open the modal
-//       this.openEventModal = true;
-//       this.event_date = new Date(this.year, this.month, date).toDateString();
-//     },
-//
-//     addEvent() {
-//       if (this.event_title == '') {
-//         return;
-//       }
-//
-//       this.events.push({
-//         event_date: this.event_date,
-//         event_title: this.event_title,
-//         event_theme: this.event_theme
-//       });
-//
-//       console.log(this.events);
-//
-//       // clear the form data
-//       this.event_title = '';
-//       this.event_date = '';
-//       this.event_theme = 'blue';
-//
-//       //close the modal
-//       this.openEventModal = false;
-//     },
-//
-//     getNoOfDays() {
-//       let daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
-//
-//       // find where to start calendar day of week
-//       let dayOfWeek = new Date(this.year, this.month).getDay();
-//       let blankdaysArray = [];
-//       for ( var i=1; i <= dayOfWeek; i++) {
-//         blankdaysArray.push(i);
-//       }
-//
-//       let daysArray = [];
-//       for ( var i=1; i <= daysInMonth; i++) {
-//         daysArray.push(i);
-//       }
-//
-//       this.blankdays = blankdaysArray;
-//       this.no_of_days = daysArray;
-//     }
-//   }
-// }
 </script>
