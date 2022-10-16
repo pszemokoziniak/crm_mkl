5f6cff043ef1fcc0ecc196d52f363d73b5bde210
pszemo <pszemo.koziniak@gmail.com>
2022-10-16 17:43:00 +0200

    Dashboard View

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use App\Models\Organization;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard/Index');
+
+//        $organizations = Contact::join('organizations', 'contacts.organization_id', 'organizations.id')->get();
+//        dd($organizations);
+
+        return Inertia::render('Dashboard/Index', [
+            'filters' => Request::all('search', 'trashed'),
+            'organizations' => Organization::join('contacts', 'organizations.kierownikBud_id', '=', 'contacts.id')
+                ->where('contacts.user_id', Auth::id())
+                ->get(['organizations.id','organizations.nazwaBud','organizations.kierownikBud_id', 'contacts.user_id']),
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,19 +1,111 @@
 <template>
   <div>
-    <Head title="Dashboard" />
-    <h1 class="mb-8 text-3xl font-bold">Witaj </h1>
-    <!-- <p class="mb-8 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo-500 hover:text-orange-600 underline" href="https://inertiajs.com">Inertia.js</a> works.</p> -->
+    <Head title="Budowa" />
+    <h1 class="mb-8 text-3xl font-bold">Twoje Budowy KCP</h1>
+    <div class="flex items-center justify-between mb-6">
+      <search-filter v-model="form.search" class="mr-4 w-full max-w-md" @reset="reset">
+        <label class="block text-gray-700">Trashed:</label>
+        <select v-model="form.trashed" class="form-select mt-1 w-full">
+          <option :value="null" />
+          <option value="with">test1</option>
+          <option value="only">test2</option>
+        </select>
+      </search-filter>
+      <Link class="btn-indigo" href="/budowy/create">
+        <span>Utwórz</span>
+        <span class="hidden md:inline">&nbsp;Budowę</span>
+      </Link>
+    </div>
+    <div class="bg-white rounded-md shadow overflow-x-auto">
+      <table class="w-full whitespace-nowrap">
+        <thead>
+        <tr class="text-left font-bold">
+          <th class="pb-4 pt-6 px-6">Nazwa</th>
+          <th class="pb-4 pt-6 px-6">Ilość Pracowników</th>
+          <th class="pb-4 pt-6 px-6" colspan="2">Miasto</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="organization in organizations" :key="organization.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+          <td class="border-t">
+            <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/building/${organization.id}/time-sheet`">
+              {{ organization.nazwaBud }}
+              <icon v-if="organization.deleted_at" name="trash" class="flex-shrink-0 ml-2 w-3 h-3 fill-gray-400" />
+            </Link>
+          </td>
+          <td class="border-t">
+            <Link class="flex items-center px-6 py-4" :href="`/building/${organization.id}/time-sheet`" tabindex="-1">
+              <div v-if="organization.country">
+                {{ organization.country.name}}
+              </div>
+            </Link>
+          </td>
+          <td class="border-t">
+            <Link class="flex items-center px-6 py-4" :href="`/building/${organization.id}/time-sheet`" tabindex="-1">
+              <div v-if="organization.kierownikBud_id">
+                {{ organization.kierownikBud_id.last_name }} {{ organization.kierownikBud_id.first_name }}
+              </div>
+            </Link>
+          </td>
+          <td class="w-px border-t">
+            <Link class="flex items-center px-4" :href="`/building/${organization.id}/time-sheet`" tabindex="-1">
+              <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
+            </Link>
+          </td>
+        </tr>
+        <tr v-if="organizations.length === 0">
+          <td class="px-6 py-4 border-t" colspan="4">Brak danych.</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+<!--    <pagination class="mt-6" :links="organizations.links" />-->
   </div>
 </template>
 
 <script>
-import { Head } from '@inertiajs/inertia-vue3'
+import { Head, Link } from '@inertiajs/inertia-vue3'
+import Icon from '@/Shared/Icon'
+import pickBy from 'lodash/pickBy'
 import Layout from '@/Shared/Layout'
+import throttle from 'lodash/throttle'
+import mapValues from 'lodash/mapValues'
+// import Pagination from '@/Shared/Pagination'
+import SearchFilter from '@/Shared/SearchFilter'
 
 export default {
   components: {
     Head,
+    Icon,
+    Link,
+    // Pagination,
+    SearchFilter,
   },
   layout: Layout,
+  props: {
+    filters: Object,
+    organizations: Object,
+  },
+  data() {
+    return {
+      form: {
+        search: this.filters.search,
+        trashed: this.filters.trashed,
+      },
+    }
+  },
+  watch: {
+    form: {
+      deep: true,
+      handler: throttle(function () {
+        this.$inertia.get('/budowy', pickBy(this.form), { preserveState: true })
+      }, 150),
+    },
+  },
+  methods: {
+    reset() {
+      this.form = mapValues(this.form, () => null)
+    },
+  },
 }
 </script>
