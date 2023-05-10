ec45938285d45051c8e9c0f1c2e236aa444b5491
MarcinGladkowski <marcingladkowski@gmail.com>
2023-05-10 07:11:04 +0200

    time-sheet: fix - query to review

--- app/Services/BuildTimeShiftCreator.php
+++ app/Services/BuildTimeShiftCreator.php
@@ -128,14 +128,15 @@ class BuildTimeShiftCreator
 
     private function getAllWorkersOnBuild(int $build, CarbonPeriod $date): Collection
     {
-        return DB::table('contacts', 'c')
+        $query = DB::table('contacts', 'c')
             ->join('contact_work_dates', 'c.id', '=', 'contact_work_dates.contact_id')
-            ->where('contact_work_dates.organization_id', $build)
-            ->whereDate(column: 'start', operator: '>=', value: $date->first()->format('Y-m-d'))
-            ->orWhereDate(column: 'start', operator: '<=', value: $date->first()->format('Y-m-d'))
-            ->whereDate(column: 'end', operator: '>=', value: $date->first()->format('Y-m-d'))
-            ->orWhereNull(column: 'end')
-            ->get();
+            ->where('contact_work_dates.organization_id', $build);
+//            ->whereDate(column: 'start', operator: '>=', value: $date->first()->format('Y-m-d'))
+//            ->orWhereDate(column: 'start', operator: '<=', value: $date->first()->format('Y-m-d'))
+//            ->whereDate(column: 'end', operator: '>=', value: $date->first()->format('Y-m-d'))
+//            ->orWhereNull(column: 'end');
+
+        return $query->get();
     }
 
     private function getWorkersOnBuildShifts(int $build, CarbonPeriod $period): Collection
