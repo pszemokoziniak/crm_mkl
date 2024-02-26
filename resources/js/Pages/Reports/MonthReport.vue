158444852c2212051f87f074a1919c7462c52ba6
MarcinGladkowski <marcingladkowski@gmail.com>
2024-02-26 20:15:51 +0100

    kcp_to_excel: hardcoded month for select

--- resources/js/Pages/Reports/MonthReport.vue
+++ resources/js/Pages/Reports/MonthReport.vue
@@ -2,23 +2,40 @@
   <div>
     <Head title="Raporty" />
     <h1 class="mb-8 text-3xl font-bold">Raport miesięczny</h1>
+
+    <div class="flex items-center justify-between mb-6">
+      <select-input v-model="month" class="pb-8 pr-6 w-full lg:w-1/5" label="Miesiąc">
+        <option v-for="month in months" :value="month">{{ month }}</option>
+      </select-input>
+    </div>
+    <a target="_self" :href="`/building/time-sheet/general-report`" class="btn-indigo py-2 px-4 rounded inline-flex items-center">
+      <DocumentDownloadIcon class="h-5 w-5 text-blue-500" />
+      <span>Pobierz</span>
+    </a>
   </div>
 </template>
 
 <script>
 import { Head } from '@inertiajs/inertia-vue3'
 import Layout from '@/Shared/Layout'
+import {DocumentDownloadIcon} from '@heroicons/vue/solid'
+import SelectInput from "@/Shared/SelectInput.vue";
 
 export default {
   components: {
+    SelectInput,
+    DocumentDownloadIcon,
     Head,
   },
   layout: Layout,
   props: {
+    months: Array,
   },
   data() {
     return {
-
+      months: [
+        '2024-01'
+      ]
     }
   }
 }
