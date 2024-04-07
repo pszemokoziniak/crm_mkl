c61a048b24067cfa32309ab14f4baf3a2b29db85
MarcinGladkowski <marcingladkowski@gmail.com>
2024-04-07 09:46:26 +0200

    tools(create): support in creating form

--- resources/js/Pages/Narzedzia/Create.vue
+++ resources/js/Pages/Narzedzia/Create.vue
@@ -15,12 +15,12 @@
 
           <div class="pb-8 pr-6 w-full">
             <div class="form-label">Zdjęcia</div>
-            <dropzone v-model="form.photos"></dropzone>
+            <dropzone v-model="form.photos" />
           </div>
 
           <div class="pb-8 pr-6 w-full">
             <div class="form-label">Dokumenty</div>
-            <dropzone v-model="form.documents"></dropzone>
+            <dropzone v-model="form.documents" />
           </div>
         </div>
         <div class="flex items-center justify-end px-8 py-4 bg-gray-50 border-t border-gray-100">
@@ -38,6 +38,7 @@ import TextInput from '@/Shared/TextInput'
 import LoadingButton from '@/Shared/LoadingButton'
 import DateInput from '@/Shared/DateInput.vue'
 import Dropzone from '@/Shared/Dropzone.vue'
+import axios from 'axios'
 
 export default {
   components: {
@@ -68,16 +69,43 @@ export default {
   computed() {
     this.form.ilosc_magazyn = this.form.ilosc_all ? this.form.ilosc_all : 0
   },
-  watch: {
-    form: {
-      handler() {
-        console.log(this.form)
-      }
-    },
-  },
   methods: {
     store() {
-      this.form.post('/narzedzia')
+      this.form
+        .transform((data) => ({
+          ...data,
+          photos: data.photos.filter(file => file.deleted !== true),
+          documents: data.documents.filter(file => file.deleted !== true),
+        }))
+        .post('/narzedzia', {
+          onBefore: () => {
+            /**
+               * Send files to delete because is impossible to add property `deleted`
+               * to File object. Backend also doesn't get it.
+               */
+            const photosToDelete = this
+              .form
+              .photos
+              .filter(file => file.deleted === true)
+              .map(file => file.name)
+
+            const documentsToDelete = this
+              .form
+              .documents
+              .filter(file => file.deleted === true)
+              .map(file => file.name)
+
+            const filesToDelete = [...photosToDelete, ...documentsToDelete]
+
+            if (filesToDelete.length > 0) {
+              axios.delete(`/narzedzia/${this.narzedzia.id}/file`, {
+                data: {
+                  files: filesToDelete,
+                },
+              })
+            }
+          },
+        })
     },
   },
 }
