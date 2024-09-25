a97d7575aaa35a8e99a86d8941621ca90287071a
pszemo <pszemo.koziniak@gmail.com>
2024-09-25 22:46:44 +0200

    add(). uprawnienia + fix according to statment

--- kcp_excel_tests/spreadsheet_excel_draft.php
+++ kcp_excel_tests/spreadsheet_excel_draft.php
@@ -117,9 +117,7 @@ foreach ($workersMonthTimeShifts as $workerId => $workerShifts) {
     $activeWorksheet->setCellValue('C'. $rowNumber, 'czas pracy od/do');
 
     $activeWorksheet->setCellValue('C'. $workingHoursRow, 'czas pracy');
-//    $activeWorksheet->setCellValue('C'. $paidFor, 'płacone za');
-
-
+    $activeWorksheet->setCellValue('C'. $paidFor, 'płacone za');
 
     foreach ($workerShifts as $key => $shift) {
 
