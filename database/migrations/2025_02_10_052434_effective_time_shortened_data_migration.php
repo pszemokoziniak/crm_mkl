c9369920edf582b51855e747d5af9ef8e7d6ec87
MarcinGladkowski <marcingladkowski@gmail.com>
2025-02-10 06:51:55 +0100

    reduced_time: add migrations

---
+++ database/migrations/2025_02_10_052434_effective_time_shortened_data_migration.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EffectiveTimeShortenedDataMigration extends Migration
+{
+    /**
+     * DEBUG SQL:
+     *
+     * SELECT
+     * work_from,
+     * work_to,
+     * effective_work_time,
+     * TIMEDIFF(work_to, work_from) AS work_diff,
+     * TIME(TIMEDIFF(work_to, work_from)) > TIME(effective_work_time) AS is_overtime
+     * FROM building_time_sheets;
+     *
+     */
+    public function up(): void
+    {
+        DB::table('building_time_sheets')->sql("
+            UPDATE building_time_sheets
+            SET reduced_working_hours = 1
+            WHERE TIME(TIMEDIFF(work_to, work_from)) > TIME(effective_work_time);
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::table('building_time_sheets')->sql("
+            UPDATE building_time_sheets
+            SET reduced_working_hours = 0
+        ");
+    }
+}
